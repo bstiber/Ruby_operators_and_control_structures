0668198994d55ca9702ddb437f0c345d76088f2f
Bill Stiber <billstiber@gmail.com>
2016-05-27 20:01:55 -0400

    add new file operators called car_speed

---
+++ car_speed.rb
@@ -0,0 +1,11 @@
+# set a variable called "too_fast" equal to "true" if the car speed is faster
+# than the speed_limit
+
+car_speed = 65
+speed_limit = 60
+too_fast = true
+
+if car_speed > speed_limit
+   too_fast
+   puts "true"
+end
