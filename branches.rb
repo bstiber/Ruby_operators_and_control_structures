df6e5bf50078cbd8ba2ba631e9f09285a78be93f
Bill Stiber <billstiber@gmail.com>
2016-05-27 22:59:46 -0400

    add more code and function to the code and more interactivity

--- branches.rb
+++ branches.rb
@@ -3,6 +3,15 @@ name  = gets.chomp
 
 if name == "Bill".downcase
   puts "#{name.upcase}!, Thats my name."
-else
+elsif
   puts "hello #{name}"
+end
+
+print "#{name}, would you like to see your name backwards? Enter 'yes' or 'no' "
+answer = gets.chomp
+
+if answer == "yes"
+  puts "#{name.reverse.downcase}, it is..... #{name.capitalize}"
+else answer == "no"
+  puts "#{name.upcase}, it's your loss...."
 end
