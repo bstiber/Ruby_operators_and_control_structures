8370f3d89f1bf800428dd6594891f27a12737913
Bill Stiber <billstiber@gmail.com>
2016-05-27 23:16:53 -0400

    add more interactivity to branches

--- branches.rb
+++ branches.rb
@@ -4,14 +4,16 @@ name  = gets.chomp
 if name == "Bill".downcase
   puts "#{name.upcase}!, Thats my name."
 elsif
-  puts "hello #{name}"
+  puts "hello #{name.capitalize}."
 end
 
-print "#{name}, would you like to see your name backwards? Enter 'yes' or 'no' "
+print "#{name.capitalize}, would you like to see your name backwards? Enter 'yes' or 'no'."
 answer = gets.chomp
 
-if answer == "yes"
-  puts "#{name.reverse.downcase}, it is..... #{name.capitalize}"
-else answer == "no"
+if answer == "yes" || answer == "y"
+  puts "#{name.reverse.downcase}, it is..... #{name.capitalize}."
+elsif answer == "no" ||  answer == "n"
   puts "#{name.upcase}, it's your loss...."
+else answer != "yes" || answer != "no"
+  puts "maybe some other time."
 end
