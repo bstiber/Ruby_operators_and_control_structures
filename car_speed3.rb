0c433f9570a5f5f1cd0ef2f7aa91fe38d3feea04
Bill Stiber <billstiber@gmail.com>
2016-05-27 23:48:29 -0400

    add a new file with changes to car_speed

---
+++ car_speed3.rb
@@ -0,0 +1,15 @@
+# using elsif statement, modify the code below to check whether or not the
+# car_speed is equal to the speed_limit.   If it is, set the variable
+# "going_speed_limit" to true.
+
+car_speed = 55
+speed_limit = 55
+going_speed_limit = 55
+
+if car_speed == speed_limit
+  going_speed_limit == true
+elsif car_speed < speed_limit
+  going_speed_limit = false
+else
+  going_speed_limit = false
+end
